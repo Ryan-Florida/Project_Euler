36d1189ad1ace07c07ed0e307569604b502b5eef
Ryan Florida <rjf2u@mtmail.mtsu.edu>
2018-06-04 19:27:57 -0500

    Solution for 'Consecutive Prime Sum' problem submitted.

---
+++ Consecutive_Prime_Sum.py
@@ -0,0 +1,26 @@
+from math import sqrt, ceil
+
+def IsPrime(n: int) -> bool:
+    if n == 2:
+        return True
+    elif n%2 == 0 or n < 2:
+        return False
+
+    for i in range(3, ceil(sqrt(n)) + 1, 2):
+        if n%i == 0:
+            return False
+
+    return True
+
+def main():
+    boundary_val = 1000000
+    num_list     = list(filter(lambda x: IsPrime(x), range(5000)))
+    list_of_sums = []
+
+    for i in range(len(num_list)):
+        sum_of_elements = sum(num_list[:i + 1])
+        if IsPrime(sum_of_elements) and sum_of_elements < boundary_val:
+            list_of_sums.append(sum_of_elements)
+
+    print(list_of_sums[-1])
+main()
